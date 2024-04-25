87bb07ee15a544de20efcf19b6d46aa058c7894a
alanaevora <76540986+alanaevora@users.noreply.github.com>
2024-04-25 17:11:10 -0400

    Update calibration.py
    adding amp difference to plot

--- VibePy/calibration.py
+++ VibePy/calibration.py
@@ -227,6 +227,7 @@ def main(fs, original_filename, device_num, input_channel, output_channel,
 
     # Determine peak
     measured_peak2 = round(max(abs(stim2)), 2)
+    diff = round(20*np.log10(measured_peak2/target_amp),1)
 
     # Plot
     plot_waveforms(
@@ -235,7 +236,8 @@ def main(fs, original_filename, device_num, input_channel, output_channel,
         f'Waveform of Stimulus.',
         'Waveform of Recorded, Calibrated Stimulus.\n'
         f'Measured peak: {measured_peak2}.'
-        f'Target: {target_amp}.')
+        f'Target: {target_amp}.'
+        f' Difference (dB): {diff}')
 
     stim1_freq, stim1_amp = get_amplitude_spectrum(amp_adjusted_stim1, fs, fft, [lo, hi]) # stimulus
     stim2_freq, stim2_amp = get_amplitude_spectrum(stim2, fs, fft, [lo, hi]) # calibrated stimulus
